ab002a92f4bd1c7d086f3c10d9e2313549f25779
Jonathan Henrique <31408451+jhowbhz@users.noreply.github.com>
2021-06-15 01:47:59 -0300

    Update msgHndlr.js

--- msgHndlr.js
+++ msgHndlr.js
@@ -51,9 +51,9 @@ module.exports = msgHandler = async (client, message) => {
             "ffmpegPath": "/usr/bin/ffmpeg",        // FFmpeg binary location
             "outputPath": "./media/to/mp3",    // Output file location (default: the home directory)
             "youtubeVideoQuality": "highestaudio",  // Desired video quality (default: highestaudio)
-            "queueParallelism": 2,                  // Download parallelism (default: 1)
-            "progressTimeout": 2000,                // Interval in ms for the progress reports (default: 1000)
-            "allowWebm": false,                      // Enable download from WebM sources (default: false)
+            "queueParallelism": 3,                  // Download parallelism (default: 1)
+            "progressTimeout": 5000,                // Interval in ms for the progress reports (default: 1000)
+            "allowWebm": true,                      // Enable download from WebM sources (default: false)
             "outputOptions" : ["-af", "silenceremove=1:0:-50dB", '-movflags','frag_keyframe+empty_moov'], // Additional output options passend to ffmpeg
             "youtubeVideoQuality": 'lowest'
         });
